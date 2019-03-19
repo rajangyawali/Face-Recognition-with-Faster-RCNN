71bbfd0a594e71cf7da0b86bd59d8ea8c6dfc2ef
Rajan Gyawali <rajangyawali93@gmail.com>
2019-03-19 11:38:07 +0545

    Update train.py

--- train.py
+++ train.py
@@ -382,9 +382,6 @@ if __name__ == '__main__':
 		tf.keras.layers.Conv2D(filters=64, kernel_size=[3,3], activation=tf.nn.relu),
 		tf.keras.layers.Conv2D(filters=64, kernel_size= [3,3], activation=tf.nn.relu),
 		tf.keras.layers.MaxPooling2D(pool_size=[2,2]),
-		#tf.keras.layers.Conv2D(filters=128, kernel_size=[3,3], activation=tf.nn.relu),
-		#tf.keras.layers.Conv2D(filters=128, kernel_size= [3,3], activation=tf.nn.relu),
-		#tf.keras.layers.MaxPooling2D(pool_size=[2,2]),
 		tf.keras.layers.Flatten(),
 		tf.keras.layers.Dense(512, activation=tf.nn.relu),
 		tf.keras.layers.Dense(len(labels),activation=tf.nn.softmax)
