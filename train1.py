3b5e289a928cd83095548cbc64e68a5ca3842130
Rajan Gyawali <rajangyawali93@gmail.com>
2019-04-28 23:32:22 +0545

    Update train1.py

--- train1.py
+++ train1.py
@@ -241,8 +241,8 @@ if __name__ == '__main__':
 	# 	num_images = len(imglist)
 	train_data = []
 	train_labels = []
-	faces = [ 'ram', 'shyam','krishna','hari','rita','gita','hasina','unknown']
-	labels = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+	faces = [ 'ram', 'shyam','krishna','hari','rita','gita','hasina','raman']
+	labels = [0, 1, 2, 3, 4, 5, 6, 7]
 	imglist = []
 	image_written = 0
 	for face, label in zip(faces, labels):
